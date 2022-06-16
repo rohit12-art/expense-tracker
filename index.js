46559b6f7e337a8dcb3b7090f0bbf967231c48d9
Rohit <rohitrohan25420@gmail.com>
2022-06-16 18:38:46 -0400

    first commit

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const dotenv = require('dotenv')
 const path = require('path');
 
 dotenv.config({path:'./config.env'});
-const PORT = process.env.PORT
+const PORT = process.env.PORT  || 5000
 
 //use middleware
 app.use(cors());
@@ -19,13 +19,11 @@ const conn = require('./db/connection');
 app.use(require('./routes/route'));
 
 //server production assets
-if(process.env.NODE_ENV === "production")
-{
-    app.use(express.static("client/build"));
-    app.get("*", (req,res) => {
-        res.sendFile(path.resolve(__dirname,'client','build','index.html' ))
-    });
-}
+app.use(express.static(path.join(__dirname, "/client/build")));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
+});
 
 
 app.listen(PORT, () => {
