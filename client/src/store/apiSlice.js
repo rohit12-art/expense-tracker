55fac834fdc65cb2aca86a7bf2d201473a43e1b4
Rohit <rohitrohan25420@gmail.com>
2022-06-16 18:49:37 -0400

    editted client

--- client/src/store/apiSlice.js
+++ client/src/store/apiSlice.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
-const baseURI = 'http://localhost:5000'
+const baseURI = 'https://newexpense.herokuapp.com/'
 
 export const apiSlice = createApi({
     baseQuery: fetchBaseQuery({baseUrl:baseURI}),
